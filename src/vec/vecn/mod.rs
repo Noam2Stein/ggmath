32bdb24f90e60196d49579e0a0c61278d85d9bc3
Noam2Stein <noamsteindev@gmail.com>
2024-10-03 09:13:44 +0300

    VecN: WithT
    returns the same Vec type but with a different Element

--- src/vec/vecn/mod.rs
+++ src/vec/vecn/mod.rs
@@ -37,10 +37,17 @@ pub trait VecN<T: Element, const N: usize>:
     + VecNConstSet<T, N>
     + VecNSplat<T>
 {
+    type WithT<T2: Element>: VecN<T2, N>;
 }
-impl<T: Element> VecN<T, 2> for Vec2<T> {}
-impl<T: Element> VecN<T, 3> for Vec3<T> {}
-impl<T: Element> VecN<T, 4> for Vec4<T> {}
+impl<T: Element> VecN<T, 2> for Vec2<T> {
+    type WithT<T2: Element> = Vec2<T2>;
+}
+impl<T: Element> VecN<T, 3> for Vec3<T> {
+    type WithT<T2: Element> = Vec3<T2>;
+}
+impl<T: Element> VecN<T, 4> for Vec4<T> {
+    type WithT<T2: Element> = Vec4<T2>;
+}
 
 pub trait ElementVec:
     ElementVecInner + ElementVecDefault + ElementVecConstSwizzle + ElementVecSwizzle + ElementVecSplat
