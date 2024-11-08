0438c89533fe1e8b32f3a4c1999835c9c9cc77eb
Noam2Stein <noamsteindev@gmail.com>
2024-11-08 17:58:17 +0200

    Default interface simplification

--- ggmath/src/vector/interfaces/default.rs
+++ ggmath/src/vector/interfaces/default.rs
@@ -1,5 +1,5 @@
 ggmath_proc_macros::vec_interface!(
-    Default:
+    impl Default:
 
     ScalarDefault: Scalar + Default,
 
@@ -7,14 +7,3 @@ ggmath_proc_macros::vec_interface!(
         Vector::from_array([<T as Default>::default(); N])
     }
 );
-
-use crate::vector::{ScalarCount, VecAlignment, VecLen, Vector};
-
-impl<const N: usize, T: ScalarDefault, A: VecAlignment> Default for Vector<N, T, A>
-where
-    ScalarCount<N>: VecLen<N>,
-{
-    fn default() -> Self {
-        Self::default()
-    }
-}
