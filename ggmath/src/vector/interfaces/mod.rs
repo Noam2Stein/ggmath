98f5a6a6eaa04d903f3831fcafae59158f667faf
Noam2Stein <noamsteindev@gmail.com>
2024-11-11 09:48:04 +0200

    Revert "unfinished ops"
    This reverts commit d032fc67cbfd9bc202660052595f22419b7088fa.

--- ggmath/src/vector/interfaces/mod.rs
+++ ggmath/src/vector/interfaces/mod.rs
@@ -13,7 +13,7 @@ pub(crate) mod scalar_traits {
 
 #[allow(private_bounds)]
 pub(super) trait VecLenInterfaces<const N: usize>:
-    VecLenScalar<N> + VecLenScalarDefault<N> + VecLenScalarNeg<N> + VecLenScalarNot<N>
+    VecLenScalar<N> + VecLenScalarDefault<N>
 where
     ScalarCount<N>: VecLen<N>,
 {
@@ -24,10 +24,7 @@ impl VecLenInterfaces<4> for ScalarCount<4> {}
 
 #[allow(private_bounds)]
 pub(super) trait VecAlignmentInterfaces<const N: usize>:
-    VecAlignmentScalar<N>
-    + VecAlignmentScalarDefault<N>
-    + VecAlignmentScalarNeg<N>
-    + VecAlignmentScalarNot<N>
+    VecAlignmentScalar<N> + VecAlignmentScalarDefault<N>
 where
     ScalarCount<N>: VecLen<N>,
 {
