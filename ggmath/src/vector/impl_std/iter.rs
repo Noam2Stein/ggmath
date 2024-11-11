e2f9175f0f1897e0432fcf9ec1c985858e76237e
Noam2Stein <noamsteindev@gmail.com>
2024-11-11 11:34:31 +0200

    vector: iter

---
+++ ggmath/src/vector/impl_std/iter.rs
@@ -0,0 +1,47 @@
+use super::*;
+
+impl<const N: usize, T: Scalar, A: VecAlignment> IntoIterator for Vector<N, T, A>
+where
+    ScalarCount<N>: VecLen<N>,
+{
+    type Item = T;
+    type IntoIter = <[T; N] as IntoIterator>::IntoIter;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.into_array().into_iter()
+    }
+}
+impl<'a, const N: usize, T: Scalar, A: VecAlignment> IntoIterator for &'a Vector<N, T, A>
+where
+    ScalarCount<N>: VecLen<N>,
+{
+    type Item = &'a T;
+    type IntoIter = <&'a [T; N] as IntoIterator>::IntoIter;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.as_array().iter()
+    }
+}
+impl<'a, const N: usize, T: Scalar, A: VecAlignment> IntoIterator for &'a mut Vector<N, T, A>
+where
+    ScalarCount<N>: VecLen<N>,
+{
+    type Item = &'a mut T;
+    type IntoIter = <&'a mut [T; N] as IntoIterator>::IntoIter;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.as_array_mut().iter_mut()
+    }
+}
+
+impl<'a, const N: usize, T: Scalar, A: VecAlignment> Vector<N, T, A>
+where
+    ScalarCount<N>: VecLen<N>,
+{
+    pub fn iter_ref(&self) -> <&Self as IntoIterator>::IntoIter {
+        self.into_iter()
+    }
+    pub fn iter_mut(&mut self) -> <&mut Self as IntoIterator>::IntoIter {
+        self.into_iter()
+    }
+}
--- ggmath/src/vector/impl_std/mod.rs
+++ ggmath/src/vector/impl_std/mod.rs
@@ -4,5 +4,6 @@ mod copy;
 mod debug;
 mod display;
 mod index;
+mod iter;
 mod partial_eq;
 mod scalar_ops;
