8343e21aa53156dfff25959de3f7969b5b27830f
Noam2Stein <noamsteindev@gmail.com>
2024-11-11 13:20:30 +0200

    ensure vector is construct

--- ggmath/src/vector/impl_std/mod.rs
+++ ggmath/src/vector/impl_std/mod.rs
@@ -7,3 +7,14 @@ mod index;
 mod iter;
 mod partial_eq;
 mod scalar_ops;
+
+const _: () = {
+    fn ensure_vector_is_construct<const N: usize, T: Scalar, A: VecAlignment>()
+    where
+        ScalarCount<N>: VecLen<N>,
+    {
+        fn despicable_me<JasonSegel: Construct>() {}
+
+        despicable_me::<Vector<N, T, A>>();
+    }
+};
