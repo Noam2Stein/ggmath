c5073ef07952cd4c7cc16a2ca46e15c887aaceb4
Noam2Stein <noamsteindev@gmail.com>
2024-11-11 09:34:34 +0200

    test interface fixed

--- ggmath/src/vector/interfaces/test.rs
+++ ggmath/src/vector/interfaces/test.rs
@@ -1,3 +1,4 @@
+use std::mem::MaybeUninit;
 use std::ops::*;
 
 ggmath_proc_macros::vec_interface!(
@@ -7,7 +8,12 @@ ggmath_proc_macros::vec_interface!(
 
     type Output = Vector<N, <T as Add<Rhs>>::Output, A>;
 
-    fn add(mut self, rhs: Vector<N, Rhs, A>) -> <Self as Add<Vector<N, Rhs, A>>>::Output {
-        (0..N).map(|index| self.get(index).unwrap().add(rhs.get(index).unwrap()))
+    fn add(mut self, rhs: Vector<N, Rhs, A>) -> Vector<N, <T as Add<Rhs>>::Output, A>  {
+        let mut output_array = unsafe { MaybeUninit::<[<T as Add<Rhs>>::Output; N]>::uninit().assume_init() };
+        for i in 0..N {
+            output_array[i] = self[i].add(rhs[i])
+        }
+
+        Vector::from_array(output_array)
     }
 );
