8b61350651330a3e8daa7bb5612a894692fff8da
Noam2Stein <noamsteindev@gmail.com>
2024-11-14 15:27:16 +0200

    ray

---
+++ ggmath/src/ray/mod.rs
@@ -0,0 +1,20 @@
+use crate::{
+    scalar::*,
+    vector::{alignment::*, length::*, *},
+};
+
+pub struct Ray<const N: usize, T: Scalar, A: VecAlignment>
+where
+    ScalarCount<N>: VecLen<N>,
+{
+    pub start: Vector<N, T, A>,
+    pub direction: Vector<N, T, A>,
+}
+
+pub type Ray2<T> = Ray<2, T, VecAligned>;
+pub type Ray3<T> = Ray<3, T, VecAligned>;
+pub type Ray4<T> = Ray<4, T, VecAligned>;
+
+pub type Ray2P<T> = Ray<2, T, VecPacked>;
+pub type Ray3P<T> = Ray<3, T, VecPacked>;
+pub type Ray4P<T> = Ray<4, T, VecPacked>;
