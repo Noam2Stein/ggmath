38bd1e203361616868d77f2db6ad12ecb351211c
Noam2Stein <noamsteindev@gmail.com>
2024-11-14 15:27:05 +0200

    quaternion

---
+++ ggmath/src/quaternion/mod.rs
@@ -0,0 +1,19 @@
+use crate::{
+    scalar::*,
+    vector::{alignment::*, length::*, *},
+};
+
+pub struct Quaternion<const N: usize, T: Scalar, A: VecAlignment>
+where
+    ScalarCount<N>: VecLen<N>,
+{
+    _inner: Vector<N, T, A>,
+}
+
+pub type Quat2<T> = Quaternion<2, T, VecAligned>;
+pub type Quat3<T> = Quaternion<3, T, VecAligned>;
+pub type Quat4<T> = Quaternion<4, T, VecAligned>;
+
+pub type Quat2P<T> = Quaternion<2, T, VecPacked>;
+pub type Quat3P<T> = Quaternion<3, T, VecPacked>;
+pub type Quat4P<T> = Quaternion<4, T, VecPacked>;
