e9949b55019ac669984bb3bf320f3515947f3bae
Noam2Stein <noamsteindev@gmail.com>
2024-11-14 11:03:59 +0200

    bounds struct

--- ggmath/src/bounds/mod.rs
+++ ggmath/src/bounds/mod.rs
@@ -1,1 +1,12 @@
+use crate::{
+    scalar::*,
+    vector::{alignment::VecAlignment, length::*, *},
+};
 
+pub struct Boundaries<const N: usize, T: Scalar, A: VecAlignment>
+where
+    ScalarCount<N>: VecLen<N>,
+{
+    pub center: Vector<N, T, A>,
+    pub extents: Vector<N, T, A>,
+}
