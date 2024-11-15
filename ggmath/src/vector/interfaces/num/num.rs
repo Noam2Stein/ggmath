8bf4d58341e6a3eb17bb3fc0fc2f184902c6e584
Noam2Stein <noamsteindev@gmail.com>
2024-11-15 06:57:59 +0200

    Num trait complete

--- ggmath/src/vector/interfaces/num/num.rs
+++ ggmath/src/vector/interfaces/num/num.rs
@@ -5,10 +5,21 @@ ggmath_proc_macros::vec_interface!(
 
     ScalarNum:
         Scalar +
+        ScalarDefault +
         ScalarAdd<T, Output = T> +
         ScalarSub<T, Output = T> +
         ScalarMul<T, Output = T> +
         ScalarDiv<T, Output = T> +
         ScalarRem<T, Output = T> +
-        Num,
+        ScalarAddAssign<T> +
+        ScalarSubAssign<T> +
+        ScalarMulAssign<T> +
+        ScalarDivAssign<T> +
+        ScalarRemAssign<T> +
+        PartialOrd +
+        Num +
+        NumCast +
+        NumRef +
+        NumAssign +
+        NumAssignRef,
 );
