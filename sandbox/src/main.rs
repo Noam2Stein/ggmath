f55ae79c1c1725ac55753703d3177c6d41d46a73
Noam2Stein <noamsteindev@gmail.com>
2024-11-15 06:58:43 +0200

    sandbox rect example

--- sandbox/src/main.rs
+++ sandbox/src/main.rs
@@ -1,11 +1,7 @@
-use ggmath::{matrix::*, primitive_aliases::f32::*, rectangle::Rectangle, vector::into_vec::vec2};
+use ggmath::{rectangle::*, vector::into_vec::*};
 
 fn main() {
-    //let a = fvec2((1.0, 0.5)) * 2.0;
-    //let b = fvec3((a, 4.0));
-    //let c = fvec4((b.yx(), 3.0, b.z()));
-
-    let d = Rectangle::from_min_max(vec2((1, 2)), vec2((3, 4)));
+    let d = Rect2C::from_min_max(vec2((1, 2)), vec2((3, 4)));
 
     println!("{d}");
 }
