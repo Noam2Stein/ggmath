d91a0c08214cad8c3ef976fd02211bdd9b5ffa2f
Noam2Stein <noamsteindev@gmail.com>
2024-11-17 14:35:24 +0200

    Vector from_fn

--- ggmath/src/vector/api/array.rs
+++ ggmath/src/vector/api/array.rs
@@ -4,6 +4,10 @@ impl<const N: usize, T: Scalar, A: VecAlignment> Vector<N, T, A>
 where
     ScalarCount<N>: VecLen<N>,
 {
+    pub fn from_fn(cb: impl FnMut(usize) -> T) -> Self {
+        Vector::from_array(std::array::from_fn(cb))
+    }
+
     pub fn map<TOutput: Scalar>(self, f: impl FnMut(T) -> TOutput) -> Vector<N, TOutput, A> {
         Vector::from_array(self.into_array().map(f))
     }
