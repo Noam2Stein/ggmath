96239a2daa0586cd82acb6c4edc711875ab9e938
Noam2Stein <noamsteindev@gmail.com>
2024-11-22 17:18:47 +0200

    tests: scalar testing moved into submod

--- tests/src/main.rs
+++ tests/src/main.rs
@@ -1,9 +1,10 @@
 use std::time::Instant;
 
-use ggmath_testing::test_scalar::test_scalar;
 use matrix::test_matrix;
+use scalars::test_scalars;
 
 mod matrix;
+mod scalars;
 
 fn main() {
     let start_time = Instant::now();
@@ -23,25 +24,4 @@ fn test() {
     test_scalars();
 
     test_matrix::<u32>();
-}
-
-fn test_scalars() {
-    test_scalar::<f32>();
-    test_scalar::<f64>();
-
-    test_scalar::<u8>();
-    test_scalar::<u16>();
-    test_scalar::<u32>();
-    test_scalar::<u64>();
-    test_scalar::<u128>();
-    test_scalar::<usize>();
-
-    test_scalar::<i8>();
-    test_scalar::<i16>();
-    test_scalar::<i32>();
-    test_scalar::<i64>();
-    test_scalar::<i128>();
-    test_scalar::<isize>();
-
-    test_scalar::<bool>();
 }
