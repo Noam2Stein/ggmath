6fc254921da42175787568c5bf9b85cb62914fa8
Noam2Stein <noamsteindev@gmail.com>
2024-11-23 21:41:28 +0200

    unfinished

--- ggmath/src/scalar/impl_std/cmp.rs
+++ ggmath/src/scalar/impl_std/cmp.rs
@@ -2,33 +2,96 @@ use super::*;
 
 use std::{array, cmp::Ordering};
 
-ggmath_proc_macros::vector_interface!(
-    ScalarPartialEq<Rhs: Scalar>: Scalar + PartialEq<Rhs>;
+// PartialEq, Eq
 
-    impl<ARhs: VecAlignment> PartialEq<Vector<N, Rhs, ARhs>>:
-
-    fn eq(&self, rhs: &Vector<N, Rhs, ARhs>) -> bool {
-        (0..N).all(|i| self[i].eq(&rhs[i]))
+pub trait ScalarPartialEq<Rhs: Scalar = Self>: Scalar + PartialEq<Rhs> {
+    #[inline(always)]
+    fn vector_eq<const N: usize, A: VecAlignment>(
+        vec: &Vector<N, Self, A>,
+        other: &Vector<N, Rhs, impl VecAlignment>,
+    ) -> bool
+    where
+        ScalarCount<N>: VecLen,
+    {
+        (0..N).all(|i| vec[i].eq(&other[i]))
     }
-);
+}
+
+impl<
+        const N: usize,
+        T: ScalarPartialEq<TRhs>,
+        A: VecAlignment,
+        TRhs: Scalar,
+        ARhs: VecAlignment,
+    > PartialEq<Vector<N, TRhs, ARhs>> for Vector<N, T, A>
+where
+    ScalarCount<N>: VecLen,
+{
+    #[inline(always)]
+    fn eq(&self, other: &Vector<N, TRhs, ARhs>) -> bool {
+        T::vector_eq(&self, other)
+    }
+}
 
 impl<const N: usize, T: ScalarPartialEq<T> + Eq, A: VecAlignment> Eq for Vector<N, T, A> where
     ScalarCount<N>: VecLen
 {
 }
 
-ggmath_proc_macros::vector_interface!(
-    ScalarPartialOrd: ScalarPartialEq<T> + PartialOrd<T>;
+// PartialOrd, Ord
 
-    pub impl:
+pub trait ScalarPartialOrd: ScalarPartialEq + PartialOrd {
+    #[inline(always)]
+    fn min(self, other: Self) -> Self {
+        if self > other {
+            other
+        } else {
+            self
+        }
+    }
+    #[inline(always)]
+    fn max(self, other: Self) -> Self {
+        if self > other {
+            self
+        } else {
+            other
+        }
+    }
+    #[inline(always)]
+    fn clamp(self, min: Self, max: Self) -> Self {
+        if self > max {
+            max
+        } else if self < min {
+            min
+        } else {
+            self
+        }
+    }
 
-    fn cmin(self) -> T {
-        self.iter().min_by(|a, b| a.partial_cmp(b).unwrap_or(Ordering::Equal)).unwrap_or(self[0])
+    fn vector_cmin<const N: usize, A: VecAlignment>(vec: Vector<N, Self, A>) -> Self
+    where
+        ScalarCount<N>: VecLen,
+    {
+        vec.iter()
+            .min_by(|a, b| a.partial_cmp(b).unwrap_or(Ordering::Equal))
+            .unwrap_or(vec[0])
     }
-    fn cmax(self) -> T {
-        self.iter().max_by(|a, b| a.partial_cmp(b).unwrap_or(Ordering::Equal)).unwrap_or(self[0])
+    fn vector_cmax<const N: usize, A: VecAlignment>(vec: Vector<N, Self, A>) -> Self
+    where
+        ScalarCount<N>: VecLen,
+    {
+        vec.iter()
+            .max_by(|a, b| a.partial_cmp(b).unwrap_or(Ordering::Equal))
+            .unwrap_or(vec[0])
     }
-    fn min(self, other: Vector<N, T, impl VecAlignment>) -> Self {
+
+    fn vector_min<const N: usize, A: VecAlignment>(
+        self,
+        other: Vector<N, Self, impl VecAlignment>,
+    ) -> Self
+    where
+        ScalarCount<N>: VecLen,
+    {
         Vector::from_array(array::from_fn(|i| match self[i].partial_cmp(&other[i]) {
             None => self[i],
             Some(Ordering::Less) => self[i],
@@ -36,7 +99,13 @@ ggmath_proc_macros::vector_interface!(
             Some(Ordering::Greater) => other[i],
         }))
     }
-    fn max(self, other: Vector<N, T, impl VecAlignment>) -> Self {
+    fn vector_max<const N: usize, A: VecAlignment>(
+        self,
+        other: Vector<N, Self, impl VecAlignment>,
+    ) -> Self
+    where
+        ScalarCount<N>: VecLen,
+    {
         Vector::from_array(array::from_fn(|i| match self[i].partial_cmp(&other[i]) {
             None => self[i],
             Some(Ordering::Less) => other[i],
@@ -44,7 +113,43 @@ ggmath_proc_macros::vector_interface!(
             Some(Ordering::Greater) => self[i],
         }))
     }
-    fn clamp(self, min: Vector<N, T, impl VecAlignment>, max: Vector<N, T, impl VecAlignment>) -> Self {
+    fn vector_clamp<const N: usize, A: VecAlignment>(
+        self,
+        min: Vector<N, Self, impl VecAlignment>,
+        max: Vector<N, Self, impl VecAlignment>,
+    ) -> Self
+    where
+        ScalarCount<N>: VecLen,
+    {
         self.max(min).min(max)
     }
-);
+}
+
+impl<const N: usize, T: ScalarPartialOrd, A: VecAlignment> Vector<N, T, A>
+where
+    ScalarCount<N>: VecLen,
+{
+    pub fn cmin(self) -> T {
+        T::vector_cmin(self)
+    }
+
+    pub fn cmax(self) -> T {
+        T::vector_cmax(self)
+    }
+
+    pub fn min(self, other: Vector<N, T, impl VecAlignment>) -> Self {
+        T::vector_min(self, other)
+    }
+
+    pub fn max(self, other: Vector<N, T, impl VecAlignment>) -> Self {
+        T::vector_max(self, other)
+    }
+
+    pub fn clamp(
+        self,
+        min: Vector<N, T, impl VecAlignment>,
+        max: Vector<N, T, impl VecAlignment>,
+    ) -> Self {
+        T::vector_clamp(self, min, max)
+    }
+}
--- ggmath/src/scalar/impl_std/ops.rs
+++ ggmath/src/scalar/impl_std/ops.rs
@@ -29,50 +29,49 @@ for_self_ops!($(
 )*);
 
 for_rhs_ops!($(
-        pub trait $scalar_trait<Rhs: Scalar>: Scalar + $std_trait<Rhs, Output: Scalar> {
-            fn $scalar_fn<const N: usize, A: VecAlignment>
-                (vec: Vector<N, Self, A>, rhs: Vector<N, Rhs, impl VecAlignment>) -> Vector<N, Self::Output, A>
-            where
-                ScalarCount<N>: VecLen,
-            {
-                vec.map(|c| c.$std_fn())
-            }
-        }
-
-        impl<const N: usize, T: $scalar_trait, A: VecAlignment>
-            $std_trait for Vector<N, T, A>
+    pub trait $scalar_trait<Rhs: Scalar>: Scalar + $std_trait<Rhs, Output: Scalar> {
+        fn $scalar_fn<const N: usize, A: VecAlignment>
+            (vec: Vector<N, Self, A>, rhs: Vector<N, Rhs, impl VecAlignment>) -> Vector<N, Self::Output, A>
         where
-            ScalarCount<N>: VecLen
+            ScalarCount<N>: VecLen,
         {
-            type Output = Vector<N, <T as $std_trait>::Output, A>;
-
-            fn $std_fn(self) -> Vector<N, <T as $std_trait>::Output, A> {
-                T::$scalar_fn(self)
-            }
+            Vector::from_fn(|i| vec[i].$std_fn(rhs[i]))
         }
-    )*
-    $(ggmath_proc_macros::vector_interface!(
-        $scalar_trait<Rhs: Scalar>: Scalar + $std_trait<Rhs, Output: Scalar>;
+    }
 
-        impl<ARhs: VecAlignment> $std_trait<Vector<N, Rhs, ARhs>>:
+    impl<const N: usize, T: $scalar_trait<TRhs>, A: VecAlignment, TRhs: Scalar, ARhs: VecAlignment>
+        $std_trait<Vector<N, TRhs, ARhs>> for Vector<N, T, A>
+    where
+        ScalarCount<N>: VecLen,
+    {
+        type Output = Vector<N, T::Output, A>;
 
-        type Output = Vector<N, <T as $std_trait<Rhs>>::Output, A>;
-
-        fn $std_fn(self, rhs: Vector<N, Rhs, ARhs>) -> Vector<N, <T as $std_trait<Rhs>>::Output, A> {
-            Vector::from_fn(|i| self[i].$std_fn(rhs[i]))
+        fn $std_fn(self, rhs: Vector<N, TRhs, ARhs>) -> Vector<N, T::Output, A> {
+            T::$scalar_fn(self, rhs)
         }
-    );)*
-);
-for_assign_ops!(
-    $(ggmath_proc_macros::vector_interface!(
-        $scalar_trait<Rhs: Scalar>: Scalar + $std_trait<Rhs>;
+    }
+)*);
 
-        impl<ARhs: VecAlignment> $std_trait<Vector<N, Rhs, ARhs>>:
-
-        fn $std_fn(&mut self, rhs: Vector<N, Rhs, ARhs>) {
+for_assign_ops!($(
+    pub trait $scalar_trait<Rhs: Scalar>: Scalar + $std_trait<Rhs> {
+        fn $scalar_fn<const N: usize, A: VecAlignment>
+            (vec: &mut Vector<N, Self, A>, rhs: Vector<N, Rhs, impl VecAlignment>)
+        where
+            ScalarCount<N>: VecLen,
+        {
             for i in 0..N {
-                self[i].$std_fn(rhs[i])
+                vec[i].$std_fn(rhs[i])
             }
         }
-    );)*
-);
+    }
+
+    impl<const N: usize, T: $scalar_trait<TRhs>, A: VecAlignment, TRhs: Scalar, ARhs: VecAlignment>
+        $std_trait<Vector<N, TRhs, ARhs>> for Vector<N, T, A>
+    where
+        ScalarCount<N>: VecLen,
+    {
+        fn $std_fn(&mut self, rhs: Vector<N, TRhs, ARhs>) {
+            T::$scalar_fn(self, rhs)
+        }
+    }
+)*);
