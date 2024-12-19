0dff5a4d2a65929724a0bb32e3df07133b813c91
Noam2Stein <noamsteindev@gmail.com>
2024-12-19 08:08:54 +0200

    Vector: from_array_ref, from_array_mut (for VecPacked)

--- ggmath/src/vector/core_api/array_conversion.rs
+++ ggmath/src/vector/core_api/array_conversion.rs
@@ -41,3 +41,28 @@ where
         unsafe { transmute(self) }
     }
 }
+
+impl<const N: usize, T: Scalar> Vector<N, T, VecPacked>
+where
+    ScalarCount<N>: VecLen,
+{
+    /// Converts an array reference to a vector reference.
+    ///
+    /// This is only possible with ```VecPacked``` alignment
+    /// because it guarentees the same type-layout as an array,
+    /// where as a ```VecAligned``` vector might have a larger size than an array.
+    #[inline(always)]
+    pub fn from_array_ref(array: &[T; N]) -> &Self {
+        unsafe { transmute(array) }
+    }
+
+    /// Converts a mutable array reference to a mutable vector reference.
+    ///
+    /// This is only possible with ```VecPacked``` alignment
+    /// because it guarentees the same type-layout as an array,
+    /// where as a ```VecAligned``` vector might have a larger size than an array.
+    #[inline(always)]
+    pub fn from_array_mut(array: &mut [T; N]) -> &mut Self {
+        unsafe { transmute(array) }
+    }
+}
