d837aaea94e0b55abbdc56d5ebb573b4504684cd
Noam2Stein <noamsteindev@gmail.com>
2024-12-26 07:52:01 +0200

    vector: as_bytes, as_bytes_mut, as_bytes_padded, as_bytes_padded_mut

--- ggmath/src/vector/core_api/mod.rs
+++ ggmath/src/vector/core_api/mod.rs
@@ -1,6 +1,7 @@
 use super::*;
 
 mod array_conversion;
+mod as_bytes;
 mod get;
 mod get_mut;
 mod get_ref;
@@ -10,6 +11,7 @@ mod swizzle_wrappers;
 mod with;
 
 mod builder;
-mod or_t;
 pub use builder::*;
+
+mod or_t;
 pub use or_t::*;
