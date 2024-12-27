4a4571f7b2456f2f0f389ad76d797952fe333d6c
Noam2Stein <noamsteindev@gmail.com>
2024-12-27 12:54:56 +0200

    quaternion struct fixed
    for some reason it was previously configured as generic over N. Because quaternions only represent 3D rotation they should always have a Vec4 inside.

--- ggmath/src/quaternion/mod.rs
+++ ggmath/src/quaternion/mod.rs
@@ -1,13 +1,6 @@
 use crate::{scalar::*, vector::*};
 
-pub struct Quaternion<const N: usize, T: Scalar, A: VecAlignment>(pub Vector<N, T, A>)
-where
-    ScalarCount<N>: VecLen;
+pub struct Quaternion<T: Scalar, A: VecAlignment>(pub Vector<4, T, A>);
 
-pub type Quat2<T> = Quaternion<2, T, VecAligned>;
-pub type Quat3<T> = Quaternion<3, T, VecAligned>;
-pub type Quat4<T> = Quaternion<4, T, VecAligned>;
-
-pub type Quat2P<T> = Quaternion<2, T, VecPacked>;
-pub type Quat3P<T> = Quaternion<3, T, VecPacked>;
-pub type Quat4P<T> = Quaternion<4, T, VecPacked>;
+pub type Quat<T> = Quaternion<T, VecAligned>;
+pub type QuatP<T> = Quaternion<T, VecPacked>;
