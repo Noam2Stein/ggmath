6b1e5782131fde23fd6e1019aa67a67bffd2fe59
Noam2Stein <noamsteindev@gmail.com>
2025-08-08 08:16:14 +0300

    quaternion declaration module

--- ggmath/src/quaternion/mod.rs
+++ ggmath/src/quaternion/mod.rs
@@ -5,24 +5,7 @@ use derive_where::derive_where;
 use super::*;
 
 mod conversion;
+mod declaration;
 mod new;
 mod ops;
-
-/// The quaternion type.
-///
-/// In most cases you can use this type's type aliases instead.
-/// See in [`crate::quaternion`].
-///
-/// This type is generic over scalar type and alignment,
-/// which follows the generics of [`Vector`].
-#[derive_where(Clone, Copy)]
-#[derive_where(Debug, Eq, Hash; T)]
-pub struct Quaternion<T: Scalar, A: VecAlignment> {
-    inner: Vector<4, T, A>,
-}
-
-/// Type alias to [`Quaternion<T, VecAligned>`].
-pub type Quat<T> = Quaternion<T, VecAligned>;
-
-/// Type alias to [`Quaternion<T, VecPacked>`].
-pub type QuatP<T> = Quaternion<T, VecPacked>;
+pub use declaration::*;
