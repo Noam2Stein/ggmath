f6dd0d8600a9e16a8cd605cab2869a5a75618b2a
Noam2Stein <noamsteindev@gmail.com>
2025-08-09 09:39:32 +0300

    readme fixed

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,5 @@
 #![deny(missing_docs)]
-
-//! Generic-Graphics-Math with internal optimized SIMD.
-//!
-//! - Fully generic (Vector<Len, Scalar, Alignment>...).
-//! - Optimized with SIMD internally.
-//! - Simple API (FVec2...).
-//! - Both column-major and row-major matricies.
-//! - Num traits (FloatScalar...).
-//! - Optimal for GPU structs.
-//! - Optional additional types (Rect, Ray...).
+#![doc = include_str!("../README.md")]
 
 use repetitive::repetitive;
 
