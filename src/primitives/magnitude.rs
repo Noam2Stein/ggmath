1cbca133678a85f4d0f4d048808d06bc6ab56810
Noam2Stein <noamsteindev@gmail.com>
2025-08-10 21:41:22 +0300

    remove deprecated functions
    i made breaking changes anyway

--- src/primitives/magnitude.rs
+++ src/primitives/magnitude.rs
@@ -148,68 +148,6 @@ repetitive! {
             pub fn clamp_mag_sq(self, min_mag_sq: @prim, max_mag_sq: @prim) -> Self {
                 self.with_min_mag_sq(min_mag_sq).with_max_mag_sq(max_mag_sq)
             }
-
-            // Deprecated
-
-            /// Returns the square of the vector's magnitude.
-            ///
-            /// This is faster than getting the magnitude,
-            /// because it doesn't require performing a square root.
-            #[inline(always)]
-            #[deprecated(note = "Renamed to `mag_sq`")]
-            pub fn square_mag(self) -> @prim {
-                self.mag_sq()
-            }
-
-            /// Returns a vector with the same direction as the original, but with a magnitude of `1.0`.
-            ///
-            /// Calling this on a zero vector will result in a NaN vector.
-            #[inline(always)]
-            #[cfg(@prim_is_float)]
-            #[deprecated(note = "Renamed to `normalized`")]
-            pub fn normalize(self) -> Self {
-                self.normalize()
-            }
-
-            /// Returns a vector with the same direction as the original, but with the given square magnitude.
-            ///
-            /// Calling this with a zero vector will result in a NaN vector.
-            #[inline(always)]
-            #[cfg(@prim_is_float)]
-            #[deprecated(note = "Renamed to `with_mag_sq`")]
-            pub fn with_square_mag(self, square_mag: @prim) -> Self {
-                self.with_mag_sq(square_mag)
-            }
-
-            /// Returns a vector with the same direction as the original, but with the square magnitude at least the given value.
-            ///
-            /// Calling this with a zero vector will result in a NaN vector.
-            #[inline(always)]
-            #[cfg(@prim_is_float)]
-            #[deprecated(note = "Renamed to `with_min_mag_sq`")]
-            pub fn with_min_square_mag(self, min_square_mag: @prim) -> Self {
-                self.with_min_mag_sq(min_square_mag)
-            }
-
-            /// Returns a vector with the same direction as the original, but with the square magnitude at most the given value.
-            ///
-            /// Calling this with a zero vector will result in a NaN vector.
-            #[inline(always)]
-            #[cfg(@prim_is_float)]
-            #[deprecated(note = "Renamed to `with_max_mag_sq`")]
-            pub fn with_max_square_mag(self, max_square_mag: @prim) -> Self {
-                self.with_max_mag_sq(max_square_mag)
-            }
-
-            /// Returns a vector with the same direction as the original, but with the square magnitude clamped between the given values.
-            ///
-            /// Calling this with a zero vector will result in a NaN vector.
-            #[inline(always)]
-            #[cfg(@prim_is_float)]
-            #[deprecated(note = "Renamed to `clamp_mag_sq`")]
-            pub fn clamp_square_mag(self, min_square_mag: @prim, max_square_mag: @prim) -> Self {
-                self.clamp_mag_sq(min_square_mag, max_square_mag)
-            }
         }
     }
 }
