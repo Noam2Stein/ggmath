8d6e3f0c161a8eec214602b109b79b4ee194e8ee
Noam2Stein <noamsteindev@gmail.com>
2025-08-30 12:59:12 +0300

    bool vector functions

--- src/primitives/bool.rs
+++ src/primitives/bool.rs
@@ -1,4 +1,7 @@
-use crate::vector::Scalar;
+use crate::{
+    Usize,
+    vector::{Scalar, VecAlignment, VecLen, VecLenEnum, Vector},
+};
 
 impl Scalar for bool {
     type InnerAlignedVec2 = [Self; 2];
@@ -9,4 +12,101 @@ impl Scalar for bool {
     const INNER_ALIGNED_VEC2_GARBAGE: Self::InnerAlignedVec2 = [false, false];
     const INNER_ALIGNED_VEC3_GARBAGE: Self::InnerAlignedVec3 = [false, false, false];
     const INNER_ALIGNED_VEC4_GARBAGE: Self::InnerAlignedVec4 = [false, false, false, false];
+}
+
+impl<const N: usize, A: VecAlignment> Vector<N, bool, A>
+where
+    Usize<N>: VecLen,
+{
+    /// Returns true if all components of the vector are true.
+    #[inline(always)]
+    pub const fn all_true(self) -> bool {
+        match Usize::<N>::ENUM {
+            VecLenEnum::Two => self.as_array()[0] && self.as_array()[1],
+            VecLenEnum::Three => self.as_array()[0] && self.as_array()[1] && self.as_array()[2],
+            VecLenEnum::Four => {
+                self.as_array()[0] && self.as_array()[1] && self.as_array()[2] && self.as_array()[3]
+            }
+        }
+    }
+
+    /// Returns true if all components of the vector are false.
+    #[inline(always)]
+    pub const fn all_false(self) -> bool {
+        match Usize::<N>::ENUM {
+            VecLenEnum::Two => !self.as_array()[0] && !self.as_array()[1],
+            VecLenEnum::Three => !self.as_array()[0] && !self.as_array()[1] && !self.as_array()[2],
+            VecLenEnum::Four => {
+                !self.as_array()[0]
+                    && !self.as_array()[1]
+                    && !self.as_array()[2]
+                    && !self.as_array()[3]
+            }
+        }
+    }
+
+    /// Returns true if any component of the vector is true.
+    #[inline(always)]
+    pub const fn any_true(self) -> bool {
+        match Usize::<N>::ENUM {
+            VecLenEnum::Two => self.as_array()[0] || self.as_array()[1],
+            VecLenEnum::Three => self.as_array()[0] || self.as_array()[1] || self.as_array()[2],
+            VecLenEnum::Four => {
+                self.as_array()[0] || self.as_array()[1] || self.as_array()[2] || self.as_array()[3]
+            }
+        }
+    }
+
+    /// Returns true if any component of the vector is false.
+    #[inline(always)]
+    pub const fn any_false(self) -> bool {
+        match Usize::<N>::ENUM {
+            VecLenEnum::Two => !self.as_array()[0] || !self.as_array()[1],
+            VecLenEnum::Three => !self.as_array()[0] || !self.as_array()[1] || !self.as_array()[2],
+            VecLenEnum::Four => {
+                !self.as_array()[0]
+                    || !self.as_array()[1]
+                    || !self.as_array()[2]
+                    || !self.as_array()[3]
+            }
+        }
+    }
+
+    /// Returns the number of true components in the vector.
+    #[inline(always)]
+    pub const fn count_true(self) -> usize {
+        match Usize::<N>::ENUM {
+            VecLenEnum::Two => self.as_array()[0] as usize + self.as_array()[1] as usize,
+            VecLenEnum::Three => {
+                self.as_array()[0] as usize
+                    + self.as_array()[1] as usize
+                    + self.as_array()[2] as usize
+            }
+            VecLenEnum::Four => {
+                self.as_array()[0] as usize
+                    + self.as_array()[1] as usize
+                    + self.as_array()[2] as usize
+                    + self.as_array()[3] as usize
+            }
+        }
+    }
+
+    /// Returns the number of false components in the vector.
+    #[inline(always)]
+    pub const fn count_false(self) -> usize {
+        match Usize::<N>::ENUM {
+            VecLenEnum::Two => !self.as_array()[0] as usize + !self.as_array()[1] as usize,
+            VecLenEnum::Three => {
+                !self.as_array()[0] as usize
+                    + !self.as_array()[1] as usize
+                    + !self.as_array()[2] as usize
+            }
+            VecLenEnum::Four => {
+                !self.as_array()[0] as usize
+                    + !self.as_array()[1] as usize
+                    + !self.as_array()[2] as usize
+                    + !self.as_array()[3] as usize
+            }
+        }
+    }
 }
