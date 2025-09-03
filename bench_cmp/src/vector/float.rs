826c23f8a808c202192ef1b6d9afbf5f51ee7e69
Noam2Stein <noamsteindev@gmail.com>
2025-09-03 07:15:39 +0300

    bench removed

--- bench_cmp/src/vector/float.rs
+++
@@ -1,3 +0,0 @@
-pub fn bench_cmp(slow_fns: &mut Vec<String>) {
-    slow_fns.push("FVec2A::add::<FVec2A>".to_string());
-}
--- bench_cmp/src/vector/mod.rs
+++
@@ -1,5 +0,0 @@
-mod float;
-
-pub fn bench_cmp(slow_fns: &mut Vec<String>) {
-    float::bench_cmp(slow_fns);
-}
