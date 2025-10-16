6fdfa2df990e35bac0493aad82f091530d2aa8a7
Noam2Stein <noamsteindev@gmail.com>
2025-10-16 21:39:18 +0300

    Reduce code duplication in benchmarks
    Previously, every benchmark file had its own `benchmark_unary_operator` and `benchmark_binary_operator` macros.
    Now they all use the same common macro, which can also be used for other primitives.

--- benches/f32/vec3s.rs
+++ benches/f32/vec3s.rs
@@ -2,48 +2,15 @@ use std::hint::black_box;
 
 use gungraun::{library_benchmark, library_benchmark_group, main};
 
-unary_operator_benchmark!(neg: ggmath_neg, glam_neg => -);
+#[path = "../bench_operator.rs"]
+mod bench_operator;
 
-binary_operator_benchmark!(add: ggmath_add, glam_add => +);
-binary_operator_benchmark!(sub: ggmath_sub, glam_sub => -);
-binary_operator_benchmark!(mul: ggmath_mul, glam_mul => *);
-binary_operator_benchmark!(div: ggmath_div, glam_div => /);
-binary_operator_benchmark!(rem: ggmath_rem, glam_rem => %);
+bench_unary_operator!(neg { - } for ggmath::f32::FVec3S { vec3s!(1.0, 2.0, 3.0) }, glam::Vec3 { vec3(1.0, 2.0, 3.0) });
+
+bench_binary_operator!(add { + } for ggmath::f32::FVec3S { vec3s!(1.0, 2.0, 3.0), vec3s!(4.0, 5.0, 6.0) }, glam::Vec3 { vec3(1.0, 2.0, 3.0), vec3(4.0, 5.0, 6.0) });
+bench_binary_operator!(sub { - } for ggmath::f32::FVec3S { vec3s!(1.0, 2.0, 3.0), vec3s!(4.0, 5.0, 6.0) }, glam::Vec3 { vec3(1.0, 2.0, 3.0), vec3(4.0, 5.0, 6.0) });
+bench_binary_operator!(mul { * } for ggmath::f32::FVec3S { vec3s!(1.0, 2.0, 3.0), vec3s!(4.0, 5.0, 6.0) }, glam::Vec3 { vec3(1.0, 2.0, 3.0), vec3(4.0, 5.0, 6.0) });
+bench_binary_operator!(div { / } for ggmath::f32::FVec3S { vec3s!(21.0, 12.0, 13.0), vec3s!(4.0, 5.0, 6.0) }, glam::Vec3 { vec3(21.0, 12.0, 13.0), vec3(4.0, 5.0, 6.0) });
+bench_binary_operator!(rem { % } for ggmath::f32::FVec3S { vec3s!(21.0, 12.0, 13.0), vec3s!(4.0, 5.0, 6.0) }, glam::Vec3 { vec3(21.0, 12.0, 13.0), vec3(4.0, 5.0, 6.0) });
 
 main!(library_benchmark_groups = neg, add, sub, mul, div, rem);
-
-macro_rules! unary_operator_benchmark {
-    ($group:ident: $ggmath_fn:ident, $glam_fn:ident => $op_punct:tt) => {
-        #[library_benchmark]
-        fn $ggmath_fn() -> ggmath::f32::FVec3S {
-            black_box($op_punct black_box(ggmath::vec3s!(1.0, 2.0, 3.0)))
-        }
-
-        #[library_benchmark]
-        fn $glam_fn() -> glam::Vec3 {
-            black_box($op_punct black_box(glam::vec3(1.0, 2.0, 3.0)))
-        }
-
-        library_benchmark_group!(name = $group; benchmarks = $ggmath_fn, $glam_fn);
-    };
-}
-
-use unary_operator_benchmark;
-
-macro_rules! binary_operator_benchmark {
-    ($group:ident: $ggmath_fn:ident, $glam_fn:ident => $op_punct:tt) => {
-        #[library_benchmark]
-        fn $ggmath_fn() -> ggmath::f32::FVec3S {
-            black_box(black_box(ggmath::vec3s!(1.0, 2.0, 3.0)) $op_punct black_box(ggmath::vec3s!(4.0, 5.0, 6.0)))
-        }
-
-        #[library_benchmark]
-        fn $glam_fn() -> glam::Vec3 {
-            black_box(black_box(glam::vec3(1.0, 2.0, 3.0)) $op_punct black_box(glam::vec3(4.0, 5.0, 6.0)))
-        }
-
-        library_benchmark_group!(name = $group; benchmarks = $ggmath_fn, $glam_fn);
-    };
-}
-
-use binary_operator_benchmark;
--- benches/f32/vec4.rs
+++ benches/f32/vec4.rs
@@ -2,48 +2,15 @@ use std::hint::black_box;
 
 use gungraun::{library_benchmark, library_benchmark_group, main};
 
-bench_unary_operator!(neg: ggmath_neg, glam_neg => -);
+#[path = "../bench_operator.rs"]
+mod bench_operator;
 
-bench_binary_operator!(add: ggmath_add, glam_add => +);
-bench_binary_operator!(sub: ggmath_sub, glam_sub => -);
-bench_binary_operator!(mul: ggmath_mul, glam_mul => *);
-bench_binary_operator!(div: ggmath_div, glam_div => /);
-bench_binary_operator!(rem: ggmath_rem, glam_rem => %);
+bench_unary_operator!(neg { - } for ggmath::f32::FVec4 { vec4!(1.0, 2.0, 3.0, 4.0) }, glam::Vec4 { vec4(1.0, 2.0, 3.0, 4.0) });
+
+bench_binary_operator!(add { + } for ggmath::f32::FVec4 { vec4!(1.0, 2.0, 3.0, 4.0), vec4!(5.0, 6.0, 7.0, 8.0) }, glam::Vec4 { vec4(1.0, 2.0, 3.0, 4.0), vec4(5.0, 6.0, 7.0, 8.0) });
+bench_binary_operator!(sub { - } for ggmath::f32::FVec4 { vec4!(1.0, 2.0, 3.0, 4.0), vec4!(5.0, 6.0, 7.0, 8.0) }, glam::Vec4 { vec4(1.0, 2.0, 3.0, 4.0), vec4(5.0, 6.0, 7.0, 8.0) });
+bench_binary_operator!(mul { * } for ggmath::f32::FVec4 { vec4!(1.0, 2.0, 3.0, 4.0), vec4!(5.0, 6.0, 7.0, 8.0) }, glam::Vec4 { vec4(1.0, 2.0, 3.0, 4.0), vec4(5.0, 6.0, 7.0, 8.0) });
+bench_binary_operator!(div { / } for ggmath::f32::FVec4 { vec4!(21.0, 12.0, 13.0, 14.0), vec4!(5.0, 6.0, 7.0, 8.0) }, glam::Vec4 { vec4(21.0, 12.0, 13.0, 14.0), vec4(5.0, 6.0, 7.0, 8.0) });
+bench_binary_operator!(rem { % } for ggmath::f32::FVec4 { vec4!(21.0, 12.0, 13.0, 14.0), vec4!(5.0, 6.0, 7.0, 8.0) }, glam::Vec4 { vec4(21.0, 12.0, 13.0, 14.0), vec4(5.0, 6.0, 7.0, 8.0) });
 
 main!(library_benchmark_groups = neg, add, sub, mul, div, rem);
-
-macro_rules! bench_unary_operator {
-    ($group:ident: $ggmath_fn:ident, $glam_fn:ident => $op_punct:tt) => {
-        #[library_benchmark]
-        fn $ggmath_fn() -> ggmath::f32::FVec4 {
-            black_box($op_punct black_box(ggmath::vec4!(1.0, 2.0, 3.0, 4.0)))
-        }
-
-        #[library_benchmark]
-        fn $glam_fn() -> glam::Vec4 {
-            black_box($op_punct black_box(glam::vec4(1.0, 2.0, 3.0, 4.0)))
-        }
-
-        library_benchmark_group!(name = $group; benchmarks = $ggmath_fn, $glam_fn);
-    };
-}
-
-use bench_unary_operator;
-
-macro_rules! bench_binary_operator {
-    ($group:ident: $ggmath_fn:ident, $glam_fn:ident => $op_punct:tt) => {
-        #[library_benchmark]
-        fn $ggmath_fn() -> ggmath::f32::FVec4 {
-            black_box(black_box(ggmath::vec4!(1.0, 2.0, 3.0, 4.0)) $op_punct black_box(ggmath::vec4!(5.0, 6.0, 7.0, 8.0)))
-        }
-
-        #[library_benchmark]
-        fn $glam_fn() -> glam::Vec4 {
-            black_box(black_box(glam::vec4(1.0, 2.0, 3.0, 4.0)) $op_punct black_box(glam::vec4(5.0, 6.0, 7.0, 8.0)))
-        }
-
-        library_benchmark_group!(name = $group; benchmarks = $ggmath_fn, $glam_fn);
-    };
-}
-
-use bench_binary_operator;
