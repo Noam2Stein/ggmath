35ebb480ce484480a43623016d8373d5e2358a77
Noam2Stein <noamsteindev@gmail.com>
2025-10-17 15:00:50 +0300

    i32 vector SIMD optimizations
    These may have bugs in them as they are untested.

--- src/vector/primitive_impls/i32/mod.rs
+++ src/vector/primitive_impls/i32/mod.rs
@@ -1,21 +1,15 @@
 use crate::{Scalar, SimdBehaviour};
 
+#[cfg(target_feature = "sse2")]
+mod b128_sse2;
+
+#[cfg(not(target_feature = "sse2"))]
+mod b128_fallback;
+
 impl Scalar for i32 {}
 
 // IVec2 is only 64 bits long, so it doesn't benefit from SIMD
 
 impl SimdBehaviour<2> for i32 {
     type VectorRepr = [i32; 2];
-}
-
-// TODO: add SIMD optimizations
-
-impl SimdBehaviour<3> for i32 {
-    type VectorRepr = [i32; 3];
-}
-
-// TODO: add SIMD optimizations
-
-impl SimdBehaviour<4> for i32 {
-    type VectorRepr = [i32; 4];
 }
