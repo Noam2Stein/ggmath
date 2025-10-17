76a5dcfdafe26fb54f26853c2f0c54aa9a631014
Noam2Stein <noamsteindev@gmail.com>
2025-10-17 20:25:58 +0300

    Add tests for vector int API

--- tests/mod.rs
+++ tests/mod.rs
@@ -1,15 +1,13 @@
+#![allow(arithmetic_overflow)]
+
 mod vector;
 
 macro_rules! assert_panic {
     ($e:expr, $($arg:tt)*) => {{
-        let _ = std::panic::set_hook(Box::new(|_| {}));
-        let result = std::panic::catch_unwind(|| { let _ = $e; });
-        let _ = std::panic::take_hook();
-
-        match result {
+        match std::panic::catch_unwind(|| { let _ = $e; }) {
             Ok(_) => panic!("function did not panic as expected: {}", format_args!($($arg)*)),
-            Err(_) => {}
-        }
+            Err(_) => {},
+        };
     }};
 }
 
@@ -23,5 +21,12 @@ macro_rules! assert_debug_panic {
     }};
 }
 
+macro_rules! assert_eq_debug_panic {
+    ($a:expr, $b:expr, $($arg:tt)*) => {
+        assert_debug_panic!(assert_eq!($a, $b, $($arg)*), $($arg)*)
+    };
+}
+
 use assert_debug_panic;
+use assert_eq_debug_panic;
 use assert_panic;
