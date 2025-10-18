cee98ba9ab8e07712486d4b36e27206952f73dae
Noam2Stein <noamsteindev@gmail.com>
2025-10-18 12:57:13 +0300

    u32 SIMD optimizations

--- src/vector/primitive_impls/u32/mod.rs
+++ src/vector/primitive_impls/u32/mod.rs
@@ -1,21 +1,15 @@
 use crate::{Scalar, SimdBehaviour};
 
+#[cfg(target_feature = "sse2")]
+mod b128_sse2;
+
+#[cfg(not(target_feature = "sse2"))]
+mod b128_fallback;
+
 impl Scalar for u32 {}
 
 // UVec2 is only 64 bits long, so it doesn't benefit from SIMD
 
 impl SimdBehaviour<2> for u32 {
     type VectorRepr = [u32; 2];
-}
-
-// TODO: add SIMD optimizations
-
-impl SimdBehaviour<3> for u32 {
-    type VectorRepr = [u32; 3];
-}
-
-// TODO: add SIMD optimizations
-
-impl SimdBehaviour<4> for u32 {
-    type VectorRepr = [u32; 4];
 }
