cb9c4d43164d11700b632546c84ea61bf8270b56
Noam2Stein <noamsteindev@gmail.com>
2025-10-27 05:12:59 +0200

    Update rsshader support to match rsshader

--- src/rsshader.rs
+++ src/rsshader.rs
@@ -1,4 +1,7 @@
-use rsshader::reflection::{PrimitiveType, ShaderType, VectorType};
+use rsshader::{
+    ir::{LengthIr, TypeIr, VectorIr},
+    reflection::{PrimitiveType, ShaderType, VectorType},
+};
 
 use crate::{Scalar, Simdness, SupportedVecLen, VecLen, Vector};
 
@@ -6,15 +9,15 @@ impl<const N: usize, T: Scalar + PrimitiveType, S: Simdness> ShaderType for Vect
 where
     VecLen<N>: SupportedVecLen,
 {
-    const IR: rsshader::ir::Type = rsshader::ir::Type::Vector(rsshader::ir::Vector {
+    const IR: TypeIr = TypeIr::Vector(VectorIr {
         length: match N {
-            2 => rsshader::ir::Length::Two,
-            3 => rsshader::ir::Length::Three,
-            4 => rsshader::ir::Length::Four,
+            2 => LengthIr::Two,
+            3 => LengthIr::Three,
+            4 => LengthIr::Four,
             _ => unreachable!(),
         },
         primitive: match T::IR {
-            rsshader::ir::Type::Primitive(primitive) => primitive,
+            TypeIr::Primitive(primitive) => primitive,
             _ => unreachable!(),
         },
     });
