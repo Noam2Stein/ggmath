07813def698830454c86f6470fe461e7bb6f8189
Noam2Stein <noamsteindev@proton.me>
2026-03-28 13:13:47 +0300

    Add matrix inverse functions

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -23,6 +23,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
   - `is_nan`
   - `is_finite`
   - `determinant`
+  - `inverse`
+  - `try_inverse`
+  - `inverse_or`
+  - `inverse_or_zero`
   - `recip`
   - `abs`
 
--- src/matrix/float.rs
+++ src/matrix/float.rs
@@ -1,3 +1,5 @@
+use core::convert::identity;
+
 use crate::{
     Alignment, Length, Matrix, Scalar, SupportedLength, Vector,
     num_primitive::PrimitiveFloat,
@@ -90,6 +92,227 @@ where
             }
             _ => unreachable!(),
         }
+    }
+
+    #[inline(always)]
+    #[track_caller]
+    fn generic_inverse<Output, W, C>(&self, wrap_result: W, check_determinant: C) -> Output
+    where
+        W: FnOnce(Self) -> Output,
+        C: FnOnce(T) -> Result<(), Output>,
+    {
+        match N {
+            2 => {
+                // SAFETY: Because `N == 2`, `Matrix<N, T, A>` is `Matrix<2, T, A>`.
+                let mat = unsafe { transmute_ref::<Matrix<N, T, A>, Matrix<2, T, A>>(self) };
+
+                let determinant = mat.determinant();
+                if let Err(error) = check_determinant(determinant) {
+                    return error;
+                }
+
+                let determinant_recip = determinant.recip();
+                let result = Matrix::<2, T, A>::from_column_array(&[
+                    mat.y_axis.y * determinant_recip,
+                    mat.x_axis.y * -determinant_recip,
+                    mat.y_axis.x * -determinant_recip,
+                    mat.x_axis.x * determinant_recip,
+                ]);
+
+                // SAFETY: Because `N == 2`, `Matrix<2, T, A>` is `Matrix<N, T, A>`.
+                wrap_result(unsafe {
+                    transmute_generic::<Matrix<2, T, A>, Matrix<N, T, A>>(result)
+                })
+            }
+            3 => {
+                // SAFETY: Because `N == 3`, `Matrix<N, T, A>` is `Matrix<3, T, A>`.
+                let mat = unsafe { transmute_ref::<Matrix<N, T, A>, Matrix<3, T, A>>(self) };
+
+                let y_cross_z = mat.y_axis.cross(mat.z_axis);
+                let z_cross_x = mat.z_axis.cross(mat.x_axis);
+                let x_cross_y = mat.x_axis.cross(mat.y_axis);
+
+                let determinant = mat.z_axis.dot(x_cross_y);
+                if let Err(error) = check_determinant(determinant) {
+                    return error;
+                }
+
+                let determinant_recip = Vector::<3, T, A>::splat(determinant.recip());
+                let result = Matrix::<3, T, A>::from_columns(&[
+                    y_cross_z * determinant_recip,
+                    z_cross_x * determinant_recip,
+                    x_cross_y * determinant_recip,
+                ])
+                .transpose();
+
+                // SAFETY: Because `N == 3`, `Matrix<3, T, A>` is `Matrix<N, T, A>`.
+                wrap_result(unsafe {
+                    transmute_generic::<Matrix<3, T, A>, Matrix<N, T, A>>(result)
+                })
+            }
+            4 => {
+                // SAFETY: Because `N == 4`, `Matrix<N, T, A>` is `Matrix<4, T, A>`.
+                let mat = unsafe { transmute_ref::<Matrix<N, T, A>, Matrix<4, T, A>>(self) };
+
+                let [m00, m01, m02, m03] = mat.x_axis.to_array();
+                let [m10, m11, m12, m13] = mat.y_axis.to_array();
+                let [m20, m21, m22, m23] = mat.z_axis.to_array();
+                let [m30, m31, m32, m33] = mat.w_axis.to_array();
+
+                let coef00 = m22 * m33 - m32 * m23;
+                let coef02 = m12 * m33 - m32 * m13;
+                let coef03 = m12 * m23 - m22 * m13;
+
+                let coef04 = m21 * m33 - m31 * m23;
+                let coef06 = m11 * m33 - m31 * m13;
+                let coef07 = m11 * m23 - m21 * m13;
+
+                let coef08 = m21 * m32 - m31 * m22;
+                let coef10 = m11 * m32 - m31 * m12;
+                let coef11 = m11 * m22 - m21 * m12;
+
+                let coef12 = m20 * m33 - m30 * m23;
+                let coef14 = m10 * m33 - m30 * m13;
+                let coef15 = m10 * m23 - m20 * m13;
+
+                let coef16 = m20 * m32 - m30 * m22;
+                let coef18 = m10 * m32 - m30 * m12;
+                let coef19 = m10 * m22 - m20 * m12;
+
+                let coef20 = m20 * m31 - m30 * m21;
+                let coef22 = m10 * m31 - m30 * m11;
+                let coef23 = m10 * m21 - m20 * m11;
+
+                let fac0 = Vector::<4, T, A>::new(coef00, coef00, coef02, coef03);
+                let fac1 = Vector::<4, T, A>::new(coef04, coef04, coef06, coef07);
+                let fac2 = Vector::<4, T, A>::new(coef08, coef08, coef10, coef11);
+                let fac3 = Vector::<4, T, A>::new(coef12, coef12, coef14, coef15);
+                let fac4 = Vector::<4, T, A>::new(coef16, coef16, coef18, coef19);
+                let fac5 = Vector::<4, T, A>::new(coef20, coef20, coef22, coef23);
+
+                let vec0 = Vector::<4, T, A>::new(m10, m00, m00, m00);
+                let vec1 = Vector::<4, T, A>::new(m11, m01, m01, m01);
+                let vec2 = Vector::<4, T, A>::new(m12, m02, m02, m02);
+                let vec3 = Vector::<4, T, A>::new(m13, m03, m03, m03);
+
+                let inv0 = vec1 * fac0 - vec2 * fac1 + vec3 * fac2;
+                let inv1 = vec0 * fac0 - vec2 * fac3 + vec3 * fac4;
+                let inv2 = vec0 * fac1 - vec1 * fac3 + vec3 * fac5;
+                let inv3 = vec0 * fac2 - vec1 * fac4 + vec2 * fac5;
+
+                let sign_a = Vector::<4, T, A>::new(
+                    T::as_from(1.0),
+                    T::as_from(-1.0),
+                    T::as_from(1.0),
+                    T::as_from(-1.0),
+                );
+                let sign_b = Vector::<4, T, A>::new(
+                    T::as_from(-1.0),
+                    T::as_from(1.0),
+                    T::as_from(-1.0),
+                    T::as_from(1.0),
+                );
+
+                let inverse = Matrix::<4, T, A>::from_columns(&[
+                    inv0 * sign_a,
+                    inv1 * sign_b,
+                    inv2 * sign_a,
+                    inv3 * sign_b,
+                ]);
+
+                let col0 = Vector::<4, T, A>::new(
+                    inverse.x_axis.x,
+                    inverse.y_axis.x,
+                    inverse.z_axis.x,
+                    inverse.w_axis.x,
+                );
+
+                let dot0 = mat.x_axis * col0;
+                let dot1 = dot0.x + dot0.y + dot0.z + dot0.w;
+
+                if let Err(error) = check_determinant(dot1) {
+                    return error;
+                }
+
+                let determinant_recip = dot1.recip();
+                let result = inverse * determinant_recip;
+
+                // SAFETY: Because `N == 4`, `Matrix<4, T, A>` is `Matrix<N, T, A>`.
+                wrap_result(unsafe {
+                    transmute_generic::<Matrix<4, T, A>, Matrix<N, T, A>>(result)
+                })
+            }
+            _ => unreachable!(),
+        }
+    }
+
+    /// Returns the inverse of `self`.
+    ///
+    /// If `self` is not invertable the result is unspecified.
+    ///
+    /// # Panics
+    ///
+    /// When assertions are enabled (see the crate documentation):
+    ///
+    /// Panics if the determinant is `0`.
+    #[must_use]
+    #[track_caller]
+    pub fn inverse(&self) -> Self {
+        #[cfg(assertions)]
+        {
+            let mut determinant_is_zero = false;
+            let result = self.generic_inverse(identity, |determinant| {
+                determinant_is_zero = determinant == T::as_from(0.0);
+                Ok(())
+            });
+
+            if determinant_is_zero {
+                panic!("matrix is not invertable");
+            }
+
+            result
+        }
+        #[cfg(not(assertions))]
+        {
+            self.generic_inverse(identity, |_| Ok(()))
+        }
+    }
+
+    /// Returns the inverse of `self` or `None` if `self` is not invertable.
+    #[must_use]
+    pub fn try_inverse(&self) -> Option<Self> {
+        self.generic_inverse(Some, |determinant| {
+            if determinant == T::as_from(0.0) {
+                Err(None)
+            } else {
+                Ok(())
+            }
+        })
+    }
+
+    /// Returns the inverse of `self` or `fallback` if `self` is not invertable.
+    #[must_use]
+    pub fn inverse_or(&self, fallback: &Self) -> Self {
+        self.generic_inverse(identity, |determinant| {
+            if determinant == T::as_from(0.0) {
+                Err(*fallback)
+            } else {
+                Ok(())
+            }
+        })
+    }
+
+    /// Returns the inverse of `self` or the zero matrix if `self` is not
+    /// invertable.
+    #[must_use]
+    pub fn inverse_or_zero(&self) -> Self {
+        self.generic_inverse(identity, |determinant| {
+            if determinant == T::as_from(0.0) {
+                Err(Self::ZERO)
+            } else {
+                Ok(())
+            }
+        })
     }
 
     /// Returns the element-wise reciprocal (inverse) of a matrix, `1 / self`.
@@ -201,7 +424,10 @@ where
 
 #[cfg(test)]
 mod tests {
-    use crate::{Matrix, Vector, test_utils::for_parameters};
+    use crate::{
+        Matrix, Vector,
+        test_utils::{assert_float_eq, assert_panic, for_parameters},
+    };
 
     #[test]
     fn test_is_nan() {
@@ -258,6 +484,170 @@ mod tests {
             assert!(!Matrix::<4, T, A>::from_columns(&[one, one, inf, one]).is_finite());
             assert!(!Matrix::<4, T, A>::from_columns(&[one, one, one, inf]).is_finite());
             assert!(!Matrix::<4, T, A>::from_columns(&[inf, inf, inf, inf]).is_finite());
+        });
+    }
+
+    #[test]
+    fn test_inverse() {
+        for_parameters!(|T: PrimitiveFloat, A, x, y, z| {
+            let _: [T; 3] = [x, y, z];
+            let [w, a, b] = [x + y, x + z, y + z];
+            let [c, d, e] = [x * 1.3, y * 0.7, z * 1.1];
+            let [f, g, h] = [w * 2.1, a * 1.9, b * 1.6];
+            let [i, j, k, l] = [c + d, c + e, d + f, f + g];
+
+            if !x.is_finite()
+                || !y.is_finite()
+                || !z.is_finite()
+                || x.abs() > 10000.0
+                || y.abs() > 10000.0
+                || z.abs() > 10000.0
+            {
+                return;
+            }
+
+            let mat = Matrix::<2, T, A>::from_column_array(&[x, y, z, w]);
+            if mat.determinant() != 0.0 {
+                assert_float_eq!(
+                    mat * mat.inverse(),
+                    Matrix::IDENTITY,
+                    abs <= Matrix::<2, T, A>::from_column_array(&[1e-5; 4])
+                        * mat.determinant().abs().log2().abs().exp2().powi(2),
+                    0.0 = -0.0
+                );
+            } else if cfg!(assertions) {
+                assert_panic!(mat.inverse());
+            }
+
+            let mat = Matrix::<3, T, A>::from_column_array(&[x, y, z, w, a, b, c, d, e]);
+            if mat.determinant() != 0.0 {
+                assert_float_eq!(
+                    mat * mat.inverse(),
+                    Matrix::IDENTITY,
+                    abs <= Matrix::<3, T, A>::from_column_array(&[1e-5; 9])
+                        * mat.determinant().abs().log2().abs().exp2().powi(2),
+                    0.0 = -0.0
+                );
+            } else if cfg!(assertions) {
+                assert_panic!(mat.inverse());
+            }
+
+            let mat = Matrix::<4, T, A>::from_column_array(&[
+                x, y, z, w, a, b, c, d, e, f, g, h, i, j, k, l,
+            ]);
+            if mat.determinant() != 0.0 {
+                assert_float_eq!(
+                    mat * mat.inverse(),
+                    Matrix::IDENTITY,
+                    abs <= Matrix::<4, T, A>::from_column_array(&[1e-5; 16])
+                        * mat.determinant().abs().log2().abs().exp2().powi(2),
+                    0.0 = -0.0
+                );
+            } else if cfg!(assertions) {
+                assert_panic!(mat.inverse());
+            }
+        });
+    }
+
+    #[test]
+    fn test_try_inverse() {
+        for_parameters!(|T: PrimitiveFloat, A, x, y, z| {
+            let _: [T; 3] = [x, y, z];
+            let [w, a, b] = [x + y, x + z, y + z];
+            let [c, d, e] = [x * 1.3, y * 0.7, z * 1.1];
+            let [f, g, h] = [w * 2.1, a * 1.9, b * 1.6];
+            let [i, j, k, l] = [c + d, c + e, d + f, f + g];
+
+            let mat = Matrix::<2, T, A>::from_column_array(&[x, y, z, w]);
+            if let Some(inverse) = mat.try_inverse() {
+                assert_float_eq!(mat.inverse(), inverse);
+            } else if cfg!(assertions) {
+                assert_panic!(mat.inverse());
+            }
+
+            let mat = Matrix::<3, T, A>::from_column_array(&[x, y, z, w, a, b, c, d, e]);
+            if let Some(inverse) = mat.try_inverse() {
+                assert_float_eq!(mat.inverse(), inverse);
+            } else if cfg!(assertions) {
+                assert_panic!(mat.inverse());
+            }
+
+            let mat = Matrix::<4, T, A>::from_column_array(&[
+                x, y, z, w, a, b, c, d, e, f, g, h, i, j, k, l,
+            ]);
+            if let Some(inverse) = mat.try_inverse() {
+                assert_float_eq!(mat.inverse(), inverse);
+            } else if cfg!(assertions) {
+                assert_panic!(mat.inverse());
+            }
+        });
+    }
+
+    #[test]
+    fn test_inverse_or() {
+        for_parameters!(|T: PrimitiveFloat, A, x, y, z| {
+            let _: [T; 3] = [x, y, z];
+            let [w, a, b] = [x + y, x + z, y + z];
+            let [c, d, e] = [x * 1.3, y * 0.7, z * 1.1];
+            let [f, g, h] = [w * 2.1, a * 1.9, b * 1.6];
+            let [i, j, k, l] = [c + d, c + e, d + f, f + g];
+
+            let mat = Matrix::<2, T, A>::from_column_array(&[x, y, z, w]);
+            if let Some(inverse) = mat.try_inverse() {
+                assert_float_eq!(mat.inverse_or(&Matrix::NAN), inverse);
+            } else if cfg!(assertions) {
+                assert_float_eq!(mat.inverse_or(&Matrix::NAN), Matrix::NAN);
+            }
+
+            let mat = Matrix::<3, T, A>::from_column_array(&[x, y, z, w, a, b, c, d, e]);
+            if let Some(inverse) = mat.try_inverse() {
+                assert_float_eq!(mat.inverse_or(&Matrix::NAN), inverse);
+            } else if cfg!(assertions) {
+                assert_float_eq!(mat.inverse_or(&Matrix::NAN), Matrix::NAN);
+            }
+
+            let mat = Matrix::<4, T, A>::from_column_array(&[
+                x, y, z, w, a, b, c, d, e, f, g, h, i, j, k, l,
+            ]);
+            if let Some(inverse) = mat.try_inverse() {
+                assert_float_eq!(mat.inverse_or(&Matrix::NAN), inverse);
+            } else if cfg!(assertions) {
+                assert_float_eq!(mat.inverse_or(&Matrix::NAN), Matrix::NAN);
+            }
+        });
+    }
+
+    #[test]
+    fn test_inverse_or_zero() {
+        for_parameters!(|T: PrimitiveFloat, A, x, y, z| {
+            let _: [T; 3] = [x, y, z];
+            let [w, a, b] = [x + y, x + z, y + z];
+            let [c, d, e] = [x * 1.3, y * 0.7, z * 1.1];
+            let [f, g, h] = [w * 2.1, a * 1.9, b * 1.6];
+            let [i, j, k, l] = [c + d, c + e, d + f, f + g];
+
+            let mat = Matrix::<2, T, A>::from_column_array(&[x, y, z, w]);
+            if let Some(inverse) = mat.try_inverse() {
+                assert_float_eq!(mat.inverse_or_zero(), inverse);
+            } else if cfg!(assertions) {
+                assert_float_eq!(mat.inverse_or_zero(), Matrix::ZERO);
+            }
+
+            let mat = Matrix::<3, T, A>::from_column_array(&[x, y, z, w, a, b, c, d, e]);
+            if let Some(inverse) = mat.try_inverse() {
+                assert_float_eq!(mat.inverse_or_zero(), inverse);
+            } else if cfg!(assertions) {
+                assert_float_eq!(mat.inverse_or_zero(), Matrix::ZERO);
+            }
+
+            let mat = Matrix::<4, T, A>::from_column_array(&[
+                x, y, z, w, a, b, c, d, e, f, g, h, i, j, k, l,
+            ]);
+            if let Some(inverse) = mat.try_inverse() {
+                assert_float_eq!(mat.inverse_or_zero(), inverse);
+            } else if cfg!(assertions) {
+                assert_float_eq!(mat.inverse_or_zero(), Matrix::ZERO);
+            }
         });
     }
 
