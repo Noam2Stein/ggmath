13e3bb3cf703a13bd01074e457d2377622ccb169
Noam2Stein <noamsteindev@proton.me>
2026-06-25 07:53:53 +0300

    refactor: Simplify `safe_arch`

--- src/backend/sse2/f32.rs
+++ src/backend/sse2/f32.rs
@@ -15,8 +15,6 @@ unsafe impl Backend<3, Aligned> for f32 {
     type Mask = __m128;
 
     safe_arch! {
-        #![target_feature(enable = "sse2")]
-
         #[inline]
         fn vector_eq(vector: &Vec3A<f32>, other: &Vec3A<f32>) -> bool {
             _mm_movemask_ps(_mm_cmpeq_ps(vector.0, other.0)) as u32 & 0b111 == 0b111
@@ -206,8 +204,6 @@ unsafe impl Backend<4, Aligned> for f32 {
     type Mask = __m128;
 
     safe_arch! {
-        #![target_feature(enable = "sse2")]
-
         #[inline]
         fn vector_eq(vector: &Vec4A<f32>, other: &Vec4A<f32>) -> bool {
             _mm_movemask_ps(_mm_cmpeq_ps(vector.0, other.0)) as u32 == 0xf
@@ -398,8 +394,6 @@ unsafe impl Backend<4, Aligned> for f32 {
 
 impl PrimitiveFloatBackend<3, Aligned> for f32 {
     safe_arch! {
-        #![target_feature(enable = "sse2")]
-
         #[inline]
         fn vector_nan_mask(vector: Vec3A<f32>) -> Mask3A<f32> {
             Mask(nan_mask(vector.0))
@@ -490,8 +484,6 @@ impl PrimitiveFloatBackend<3, Aligned> for f32 {
 
 impl PrimitiveFloatBackend<4, Aligned> for f32 {
     safe_arch! {
-        #![target_feature(enable = "sse2")]
-
         #[inline]
         fn vector_nan_mask(vector: Vec4A<f32>) -> Mask4A<f32> {
             Mask(nan_mask(vector.0))
--- src/utils/safe_arch.rs
+++ src/utils/safe_arch.rs
@@ -1,52 +1,29 @@
-/// Bypasses a language limitation to safely call intrinsics.
+/// Bypasses a language limitation to safely call functions marked with
+/// `#[target_feature(...)]` when that feature is enabled at compilation time.
 ///
-/// Calling functions annotated with `#[target_feature]` is unsafe even if the
-/// features are enabled for the current target architecture.
+/// The macro has a set of supported target features, and can be modified to
+/// support more.
 ///
-/// `safe_arch` hides the `#[target_feature]` annotation and fails compilation
-/// if the features are not enabled for the current target architecture.
-///
-/// # Examples
-///
-/// `x86_64` `sse` example:
-///
-/// ```ignore
-/// safe_arch! {
-///     #![target_feature(enable = "sse")]
-///
-///     fn useless_code() {
-///         println!("{:?}", core::arch::x86_64::_mm_set_ps(1.0, 2.0, 3.0, 4.0));
-///     }
-/// }
-///
-/// useless_code();
-/// ```
+/// The input syntax is a sequence of function declarations.
 #[allow(unused_macros)]
 macro_rules! safe_arch {
-    (
-        #![target_feature(enable = $feature:literal)]
+    ($(
+        $(#[$meta:meta])*
+        fn $f:ident($($param:ident: $Param:ty),* $(,)?) $(-> $Ret:ty)? $body:block
+    )*) => {$(
+        $(#[$meta])*
+        fn $f($($param: $Param),*) $(-> $Ret)? {
+            // Use this pattern to add more features. Make sure to keep the
+            // cfg correct, else there will be unsoundness.
+            #[cfg_attr(target_feature = "sse2", target_feature(enable = "sse2"))]
+            #[inline]
+            fn $f($($param: $Param),*) $(-> $Ret)? $body
 
-        $(
-            $(#[$meta:meta])* fn $f:ident($($param:ident: $Param:ty),* $(,)?) $(-> $Ret:ty)? $body:block
-        )*
-    ) => {
-        $(
-            $(#[$meta])*
-            fn $f($($param: $Param),*) $(-> $Ret)? {
-                #[inline]
-                #[target_feature(enable = $feature)]
-                fn $f($($param: $Param),*) $(-> $Ret)? $body
-
-                #[cfg(not(target_feature = $feature))]
-                compile_error!(concat!("target feature is not enabled"));
-
-                // SAFETY: If `$feature` is disabled, there will be a compile
-                // error.
-                unsafe { $f($($param),*) }
-            }
-        )*
-    };
+            // SAFETY: The function only requires target features that are
+            // enabled via cfg.
+            unsafe { $f($($param),*) }
+        }
+    )*};
 }
-
 #[allow(unused_imports)]
 pub(crate) use safe_arch;
