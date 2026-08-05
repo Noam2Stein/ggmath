0212a5e1d70bb97f0b731c71ea88c1ea5e90daa2
Noam2Stein <noamsteindev@proton.me>
2026-08-05 18:29:52 +0300

    feat: Add projective homogeneous conversion functions

--- src/projective.rs
+++ src/projective.rs
@@ -32,6 +32,88 @@ pub type Proj2A<T> = Projective<2, T, Aligned>;
 /// TODO
 pub type Proj3A<T> = Projective<3, T, Aligned>;
 
+impl<T, A: Alignment> Projective<2, T, A>
+where
+    T: Scalar,
+{
+    /// Reinterprets a homogeneous 3x3 matrix as a 2D projective transform.
+    ///
+    /// This is a no-op, because the representation is the same.
+    #[inline]
+    #[must_use]
+    pub const fn from_homogeneous(homogeneous: &Matrix<3, T, A>) -> Self {
+        Self(*homogeneous)
+    }
+
+    /// Reinterprets `self` as a homogeneous 3x3 matrix.
+    ///
+    /// This is a no-op, because the representation is the same.
+    #[inline]
+    #[must_use]
+    pub const fn to_homogeneous(&self) -> Matrix<3, T, A> {
+        self.0
+    }
+
+    /// Reinterprets `self` as a reference to a homogeneous 3x3 matrix.
+    ///
+    /// This is a no-op, because the representation is the same.
+    #[inline]
+    #[must_use]
+    pub const fn as_homogeneous(&self) -> &Matrix<3, T, A> {
+        &self.0
+    }
+
+    /// Reinterprets `self` as a mutable reference to a homogeneous 3x3 matrix.
+    ///
+    /// This is a no-op, because the representation is the same.
+    #[inline]
+    #[must_use]
+    pub const fn as_mut_homogeneous(&mut self) -> &mut Matrix<3, T, A> {
+        &mut self.0
+    }
+}
+
+impl<T, A: Alignment> Projective<3, T, A>
+where
+    T: Scalar,
+{
+    /// Reinterprets a homogeneous 4x4 matrix as a 3D projective transform.
+    ///
+    /// This is a no-op, because the representation is the same.
+    #[inline]
+    #[must_use]
+    pub const fn from_homogeneous(homogeneous: &Matrix<4, T, A>) -> Self {
+        Self(*homogeneous)
+    }
+
+    /// Reinterprets `self` as a homogeneous 4x4 matrix.
+    ///
+    /// This is a no-op, because the representation is the same.
+    #[inline]
+    #[must_use]
+    pub const fn to_homogeneous(&self) -> Matrix<4, T, A> {
+        self.0
+    }
+
+    /// Reinterprets `self` as a reference to a homogeneous 4x4 matrix.
+    ///
+    /// This is a no-op, because the representation is the same.
+    #[inline]
+    #[must_use]
+    pub const fn as_homogeneous(&self) -> &Matrix<4, T, A> {
+        &self.0
+    }
+
+    /// Reinterprets `self` as a mutable reference to a homogeneous 4x4 matrix.
+    ///
+    /// This is a no-op, because the representation is the same.
+    #[inline]
+    #[must_use]
+    pub const fn as_mut_homogeneous(&mut self) -> &mut Matrix<4, T, A> {
+        &mut self.0
+    }
+}
+
 impl<const N: usize, T, A: Alignment> Clone for Projective<N, T, A>
 where
     Length<N>: TwoOrThree,
