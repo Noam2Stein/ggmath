927beb9c65c839903b3f0d66e8726c79e37363b2
Noam2Stein <noamsteindev@proton.me>
2026-08-24 18:52:23 +0300

    Do the trick for `vector/wide_unsigned`

--- src/vector/wide_unsigned.rs
+++ src/vector/wide_unsigned.rs
@@ -5,39 +5,71 @@ use wide::{
 
 use crate::{Alignment, Length, SupportedLength, Vector, utils::transmute_generic};
 
-macro_rules! wide_unsigned_impl {
+macro_rules! items {
+    ($Wide:ident, $SignedWide:ty) => {
+        /// Returns the bit patterns of `self` reinterpreted as signed integers
+        /// of the same size.
+        ///
+        /// This produces the same result as [`as`] conversions, but ensures
+        /// that the bit-width remains the same.
+        ///
+        /// [`as`]: https://rust-for-c-programmers.com/ch16/16_2_primitive_casting_with_as.html
+        #[inline]
+        #[must_use]
+        pub const fn cast_signed(self) -> Vector<N, $SignedWide, A> {
+            // SAFETY: Both types accept all bit-patterns.
+            unsafe { transmute_generic::<Vector<N, $Wide, A>, Vector<N, $SignedWide, A>>(self) }
+        }
+    };
+}
+
+// Since all wide-unsigned-integer functions have names that conflict with
+// normal unsigned-integer functions, We cannot implement this API using
+// generics. Duplicating the API for each supported wide-unsigned-integer type
+// works, but then documentation shows the duplicated API, making it hard to
+// read.
+//
+// When generating documentation, Rust does not care that these items are
+// conflicting. This allows us to cheat by showing these items in a generic
+// context in documentation, but making them separate in all other cases.
+
+#[cfg(doc)]
+#[doc(hidden)]
+pub trait WideUnsigned: crate::Scalar {
+    type Signed: crate::Scalar;
+}
+
+#[cfg(doc)]
+impl<const N: usize, Wide, A: Alignment> Vector<N, Wide, A>
+where
+    Length<N>: SupportedLength,
+    Wide: WideUnsigned,
+{
+    items!(Wide, <Wide as WideUnsigned>::Signed);
+}
+
+macro_rules! impl_items {
     ($Wide:ident, $SignedWide:ident) => {
+        #[cfg(not(doc))]
         impl<const N: usize, A: Alignment> Vector<N, $Wide, A>
         where
             Length<N>: SupportedLength,
         {
-            /// Returns the bit patterns of `self` reinterpreted as signed
-            /// integers of the same size.
-            ///
-            /// This produces the same result as [`as`] conversions, but ensures
-            /// that the bit-width remains the same.
-            ///
-            /// [`as`]: https://rust-for-c-programmers.com/ch16/16_2_primitive_casting_with_as.html
-            #[inline]
-            #[must_use]
-            pub const fn cast_signed(self) -> Vector<N, $SignedWide, A> {
-                // SAFETY: Both types accept all bit-patterns.
-                unsafe { transmute_generic::<Vector<N, $Wide, A>, Vector<N, $SignedWide, A>>(self) }
-            }
+            items!($Wide, $SignedWide);
         }
     };
 }
-wide_unsigned_impl!(u8x16, i8x16);
-wide_unsigned_impl!(u8x32, i8x32);
-wide_unsigned_impl!(u16x8, i16x8);
-wide_unsigned_impl!(u16x16, i16x16);
-wide_unsigned_impl!(u16x32, i16x32);
-wide_unsigned_impl!(u32x4, i32x4);
-wide_unsigned_impl!(u32x8, i32x8);
-wide_unsigned_impl!(u32x16, i32x16);
-wide_unsigned_impl!(u64x2, i64x2);
-wide_unsigned_impl!(u64x4, i64x4);
-wide_unsigned_impl!(u64x8, i64x8);
+impl_items!(u8x16, i8x16);
+impl_items!(u8x32, i8x32);
+impl_items!(u16x8, i16x8);
+impl_items!(u16x16, i16x16);
+impl_items!(u16x32, i16x32);
+impl_items!(u32x4, i32x4);
+impl_items!(u32x8, i32x8);
+impl_items!(u32x16, i32x16);
+impl_items!(u64x2, i64x2);
+impl_items!(u64x4, i64x4);
+impl_items!(u64x8, i64x8);
 
 #[cfg(test)]
 mod tests {
