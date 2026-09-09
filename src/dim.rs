7321f668721744ba19ab18a96dc84aabfa25421b
Noam2Stein <noamsteindev@proton.me>
2026-09-09 20:24:02 +0300

    Update documentation

--- src/dim.rs
+++ src/dim.rs
@@ -1,15 +1,17 @@
-/// A marker type to restrict `const N: usize` to `2`, `3` and `4`.
+/// A marker type used to restrict `const N: usize` to specific values.
 ///
-/// `Dim<N>` and [`TwoThreeOrFour`] are markers used by types like
-/// [`Vector<N, T, A>`] to restrict `N` to `2`, `3` and `4`.
+/// [`Dim<N>`], [`TwoThreeOrFour`] and [`TwoOrThree`] are type-level markers
+/// used by types like [`Vector<N, T, A>`] to restrict `N` to `2, 3, 4` or
+/// `2, 3`.
 ///
 /// [`Vector<N, T, A>`]: crate::Vector
 pub struct Dim<const N: usize>;
 
 /// A marker trait to restrict `const N: usize` to `2`, `3` and `4`.
 ///
-/// [`Dim<N>`] and `TwoThreeOrFour` are markers used by types like
-/// [`Vector<N, T, A>`] to restrict `N` to `2`, `3` and `4`.
+/// [`Dim<N>`], [`TwoThreeOrFour`] and [`TwoOrThree`] are type-level markers
+/// used by types like [`Vector<N, T, A>`] to restrict `N` to `2, 3, 4` or
+/// `2, 3`.
 ///
 /// [`Vector<N, T, A>`]: crate::Vector
 #[expect(private_bounds)]
@@ -18,9 +20,13 @@ pub trait TwoThreeOrFour: Sealed {
     type Select<T2: Copy, T3: Copy, T4: Copy>: Copy;
 }
 
-/// An internal trait similar to [`TwoThreeOrFour`] that only accepts 2 and 3.
+/// A marker trait to restrict `const N: usize` to `2` and `3`.
 ///
-/// This is intentionally hidden from the public API.
+/// [`Dim<N>`], [`TwoThreeOrFour`] and [`TwoOrThree`] are type-level markers
+/// used by types like [`Vector<N, T, A>`] to restrict `N` to `2, 3, 4` or
+/// `2, 3`.
+///
+/// [`Vector<N, T, A>`]: crate::Vector
 pub trait TwoOrThree: TwoThreeOrFour {
     #[doc(hidden)]
     type Select<T2: Copy, T3: Copy>: Copy;
