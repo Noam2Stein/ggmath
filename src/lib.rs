46e3fbef783804222c5e2415ada674391ce86be7
Noam Stein <noamsteindev@proton.me>
2026-09-16 09:55:53 +0000

    chore: Bump version to `0.18.1` (#95)

--- src/lib.rs
+++ src/lib.rs
@@ -148,14 +148,14 @@
 //!
 //! ```toml
 //! [dependencies]
-//! ggmath = "0.18.0"
+//! ggmath = "0.18.1"
 //! ```
 //!
 //! For [`no_std`] support, enable the [`libm`] feature:
 //!
 //! ```toml
 //! [dependencies]
-//! ggmath = { version = "0.18.0", features = ["libm"] }
+//! ggmath = { version = "0.18.1", features = ["libm"] }
 //! ```
 //!
 //! # Feature flags
