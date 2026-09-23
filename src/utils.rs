330555cb50bc2fc37436d6e8a3d8da4478965b4b
Noam2Stein <noamsteindev@proton.me>
2026-09-23 13:02:33 +0300

    Remove `safe_target_feature` macro

--- src/utils.rs
+++ src/utils.rs
@@ -1,8 +1,3 @@
-#[cfg(any(
-    target_feature = "sse2",
-    all(target_arch = "aarch64", target_feature = "neon")
-))]
-pub(crate) use crate::utils::safe_target_feature::*;
 #[cfg(feature = "wide")]
 pub(crate) use crate::utils::wide_ty::*;
 pub(crate) use crate::utils::{math::*, primitive_traits::*, repr::*, specialize::*, transmute::*};
@@ -10,11 +5,6 @@ pub(crate) use crate::utils::{math::*, primitive_traits::*, repr::*, specialize:
 mod math;
 mod primitive_traits;
 mod repr;
-#[cfg(any(
-    target_feature = "sse2",
-    all(target_arch = "aarch64", target_feature = "neon")
-))]
-mod safe_target_feature;
 mod specialize;
 mod transmute;
 #[cfg(feature = "wide")]
